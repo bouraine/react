96e8b0667f81486325cadbc87cc78bb016b21a5d
bouraine <m.bouraine@live.fr>
2019-11-17 01:22:46 +0100

    should format app.js

--- react-from-scratch/src/app.js
+++ react-from-scratch/src/app.js
@@ -2,7 +2,5 @@ import * as ReactDOM from "react-dom";
 import * as React from "react";
 
 const Hello = () => <p>Hello world</p>;
-ReactDOM.render(
-    React.createElement(Hello),
-    document.getElementById("app")
-);
+
+ReactDOM.render(React.createElement(Hello), document.getElementById("app"));
